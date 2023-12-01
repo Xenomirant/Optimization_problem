c51359b61994abe4617d1e26d16f22ba0afa691c
Xenomirant <xenomirant@gmail.com>
2023-12-01 04:35:57 +0300

    fix changes

--- GD.py
+++ GD.py
@@ -88,10 +88,6 @@ class AbstractOptimizer(ABC):
         # stop if step is too small
         if np.linalg.norm(x - self._steps[-1]) < self.eps:
             return True
-
-        # stop if values increased on iteration
-        if (f_x - self._values[-1]) > 0:
-            return True
         return False
 
     def plot(self):
@@ -171,19 +167,29 @@ class AbstractOptimizer(ABC):
         while True:
 
             x = self.step(self._x, **kwargs)
-            f_x = np.array([self.substitute(self.function, self._x)], dtype=np.float32).item()
+
+            f_x = np.array([self.substitute(self.function, x)], dtype=np.float32).item()
             self._iterations += 1
 
+            # stop if values increased on iteration
+            if (f_x - self._values[-1]) > 0:
+                break
+
             if self.stop_criterion(self._iterations, x, f_x):
+                self._steps.append(x)
+                self._values.append(f_x)
                 break
 
             self._x = x
-
             self._steps.append(x)
             self._values.append(f_x)
 
+            if self._done:
+                break
+
         self._done = True
         self._path = list(zip(self._steps, self._values))
+        return
 
     def __call__(self, *args, **kwargs) -> None:
         self.minimize()
@@ -213,6 +219,8 @@ class Optimizer:
                 optimizer = Neuton
             case "ConjugateGradient"|"CG":
                 optimizer = ConjugateGradient
+            case "ConditionalGradient"|"ConG":
+                optimizer = ConditionalGradient
             case _:
                 raise ValueError("Optimizer is not yet implemented")
 
@@ -226,7 +234,13 @@ class Optimizer:
 
         expression = self.parser(expression)
 
-        self.optimizer = self.optimizer_cls(x0, expression, eps=eps, max_iter=max_iter, **kwargs)
+        if "constraints" in kwargs:
+            constraints = [self.parser(expr) for expr in kwargs.get("constraints")]
+        else:
+            constraints = []
+
+        self.optimizer = self.optimizer_cls(x0, expression, eps=eps, max_iter=max_iter,
+                                            constraints=constraints, **kwargs)
 
         self.optimizer()
 
@@ -259,10 +273,10 @@ class GD(AbstractOptimizer):
             case "armijo":
                 self._lr_type = self._armijo_lr
                 self._lr_params = kwargs.get("armijo_params", {"alpha": 1,
-                                                              "epsilon": .5,
-                                                              "theta": .5})
+                                                               "epsilon": .5,
+                                                               "theta": .5})
             case _:
-                raise NotImplemented("Method does not exist or is not implemented")
+                raise NotImplementedError("Method does not exist or is not implemented")
 
         return
 
@@ -310,7 +324,7 @@ class Neuton(AbstractOptimizer):
                 self._lr_params = {}
                 self._hessian = sym.hessian(self.function, self.vars)
             case _:
-                raise NotImplemented("Method does not exist or is not implemented")
+                raise NotImplementedError("Method does not exist or is not implemented")
 
         return
 
@@ -341,28 +355,9 @@ class ConjugateGradient(AbstractOptimizer):
                 self._bias = np.array(self.substitute(self.function, np.array([0 for var in self.vars])),
                                       dtype=np.float32).item()
             case _:
-                raise NotImplemented("Method does not exist or is not implemented")
+                raise NotImplementedError("Method does not exist or is not implemented")
 
         return
-
-    def stop_criterion(self, iteration: int, x: np.array, f_x: float) -> bool:
-
-        # stop if gradients are zero
-        if np.linalg.norm(self._num_grad) == 0:
-            return True
-
-        # stop if max_steps exceeded
-        if iteration > self.max_steps:
-            return True
-
-        # stop if step is too small
-        if np.linalg.norm(x - self._steps[-1]) < self.eps:
-            return True
-
-        # stop if values increased on iteration
-        if (f_x - self._values[-1]) > 0:
-            return True
-        return False
 
     def _quadratic_lr(self, x: np.array, *args, **kwargs):
 
@@ -373,32 +368,81 @@ class ConjugateGradient(AbstractOptimizer):
         if self._iterations >= 1:
 
             first_term = np.dot(self._quadratic_form, self._num_grad)
-            self._beta = np.dot(first_term,
-                               np.array([-self.substitute(diff, x) for diff in self._gradient], dtype=np.float32)) / \
-            np.dot(first_term, self._num_grad)
+            self._beta = (np.dot(first_term,
+                                 np.array(
+                                     [-self.substitute(diff, x) for diff in self._gradient],
+                                          dtype=np.float32))) / \
+                         (np.dot(first_term, self._num_grad))
 
         self._num_grad = np.array([self.substitute(diff, x) for diff in self._gradient], dtype=np.float32) + \
             self._beta*self._num_grad
 
-        lr = np.dot(np.dot(2*self._quadratic_form, x) + self._bias, self._num_grad) / \
-            2*np.dot(np.dot(self._quadratic_form, self._num_grad), self._num_grad)
+        lr = -(np.dot(np.dot(2*self._quadratic_form, x) + self._bias, self._num_grad)) / \
+             (2*np.dot(np.dot(self._quadratic_form, self._num_grad), self._num_grad))
 
-        print(f"lr = {lr}")
-
-        print(f"num_grad = {self._num_grad}")
+        if self._iterations >= len(self.vars) - 1:
+            self._done = True
 
         return lr
 
 
+class ConditionalGradient(AbstractOptimizer):
 
-# x1, x2, x3, x4 = sym.symbols("x1 x2 x3 x4")
+    def __init__(self, x0: np.array, function: sym.Expr, constraints: typing.List[sym.Expr], eps: float = 1e-7, max_steps: int = 1000,
+                 method: str = "linearize", **kwargs) -> None:
+
+        super().__init__(x0, function, eps, max_steps, **kwargs)
+
+        self._indices = [self.vars.index(symbol) for const in constraints for symbol in const.free_symbols]
+
+        self.constraints = [sym.lambdify(self.vars[index], constraints[n], "numpy")
+                            for n, index in enumerate(self._indices)]
+
+        match method:
+            case "linearize":
+                self._lr_type = self._fw_lr
+                self._lr_params = {}
+                # self._quadratic_form = np.array(sym.hessian(self.function, self.vars), dtype=np.float32)/2
+                # self._bias = np.array(self.substitute(self.function, np.array([0 for var in self.vars])),
+                #                       dtype=np.float32).item()
+            case _:
+                raise NotImplementedError("Method does not exist or is not implemented")
+
+        return
+
+    def check_constraints(self, x: np.array):
+        check = [self.constraints[n](x[index]) for n, index in enumerate(self._indices)]
+        if all(check):
+            return True
+        return np.argmin([self.constraints[n](x[index]) for n, index in enumerate(self._indices)])
+
+    def _fw_lr(self, x: np.array, *args, **kwargs):
+        return 2/(self._iterations + 2)
+
+    def step(self, x: np.array, **kwargs) -> np.array:
+        self._num_grad = np.array([self.substitute(diff, x) for diff in self._gradient], dtype=np.float32)
+
+        if np.linalg.norm(np.dot(self._lr_type(x, **kwargs), self._num_grad)) < self.eps:
+            self._done = True
+        x_new = x + np.dot(self._lr_type(x, **kwargs), self._num_grad)
+
+        return
+
+
+x1, x2 = sym.symbols("x1 x2")
+
+expr = x1**2 -4*x1 + x2**2  - 2*x2
+
+constr = [x1 + 0*x2 >= 0, x1 + 0*x2 <= 1, 0*x1 + x2 >= 0, 0*x1 + x2 <= 2]
 #
-# expr = x1**2 + 2*x2**2 + x1*x2 + 3*x3**2 + 100*x4**2
+x0 = np.array([0, 0])
 #
-# x0 = np.array([1, 1, 1, 1])
-#
-# opt = ConjugateGradient(x0=x0, function=expr)
+opt = ConditionalGradient(x0=x0, function=expr, constraints=constr)
+
+print(opt.step(np.array([0, 0])))
 #
 # opt.minimize()
 #
 # print(opt.path)
+#
+# opt.plot()
